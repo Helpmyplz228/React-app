494f248174f1cd18a5053409bccdbb5594c30efa
Sergey Pavlyuchenko <cergtli128@gmail.com>
2022-08-03 22:26:05 +0700

    first commit

---
+++ src/Todo/TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function TodoList(params) {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+    
+}
+
+export default TodoList
